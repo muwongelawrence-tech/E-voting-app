568ca0c78a334fcfc75e69b983348e49bebee60f
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-06-08 19:46:17 +0300

    getting the token back from the google auth

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ passport.use( new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-} , (accessToken) => {
-    console.log(accessToken);
+} , (accessToken ,refreshToken , profile , done ) => {
+    console.log('accessToken', accessToken);
+    console.log('refresh Token', refreshToken);
+    console.log('profile', profile);
 }));
 
 app.get('/', (req, res)=> {
@@ -25,6 +27,8 @@ app.get('/api/users', (req, res)=> {
 app.get('/auth/google', passport.authenticate('google', {
      scope: ['profile' , 'email']  
 }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 5000;
