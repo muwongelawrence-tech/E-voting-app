434b0ab59173ff1b7666125c87e98ea302c118e3
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-06-16 16:11:54 +0300

    configuring mongoose connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 const app = express();
 require('./routes/authRoutes')(app);
--- services/passport.js
+++ services/passport.js
@@ -1,14 +1,18 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use( new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 } , (accessToken ,refreshToken , profile , done ) => {
-    console.log('accessToken', accessToken);
-    console.log('refresh Token', refreshToken);
-    console.log('profile', profile);
+    new User({ googleId : profile.id }).save();
+    // console.log('accessToken', accessToken);
+    // console.log('refresh Token', refreshToken);
+    // console.log('profile', profile);
 }));
 
