4d4aa65bbff51c9ab397fb76dbcc98534bfa293a
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-06-25 00:15:29 +0300

    adding cookie package

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const { Passport } = require('passport');
 require('./models/User');
 require('./services/passport');
 
@@ -13,6 +14,9 @@ app.use( cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Authentication configuration adding an express session
 app.use(session({
