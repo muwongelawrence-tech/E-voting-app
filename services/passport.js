064ac158569b34478609946f54809f4088086170
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-06-24 23:50:08 +0300

    deserializing user using passport

--- services/passport.js
+++ services/passport.js
@@ -9,6 +9,13 @@ passport.serializeUser((user , done) => {
     done(null , user.id);
 });
 
+passport.deserializeUser((id , done) => {
+   User.findById(id)
+    .then(user => {
+        done(null, user);
+    })
+});
+
 passport.use( new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
