86dc602e988a011902e4e38e1e0ed7222de9f449
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-06-27 16:53:54 +0300

    configuring the cookie session

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,8 +9,14 @@ app.get('/auth/google', passport.authenticate('google', {
 
 app.get('/auth/google/callback', passport.authenticate('google'));
 
+app.get('/api/logout', (req , res) => {
+  req.logout();
+  res.send(req.user);
+});
+
 app.get('/api/current_user', (req,res) => {
   res.send(req.user);
+  //res.send(req.session)
 });
 
 };
