ecd9e76d6ff9b1554a1ae1b64886131378c3c51f
Muwonge Lawrence <85474158+muwongelawrence-tech@users.noreply.github.com>
2022-06-28 15:45:15 +0300

    Delete dev.js
    deleting dev keys

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    googleClientID : '772359068770-l1ht8o45qh3dbf85u5m1ostmad980tk1.apps.googleusercontent.com',
-    googleClientSecret:'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',
-    mongoURI: 'mongodb+srv://muwongelawrence:L%40muwo8359@emaily-dev.ebtdvvx.mongodb.net/?retryWrites=true&w=majority',
-    cookieKey: 'gfgfgfe54546656hgbgfbgbg067hth'
-
-}
