55ca1414fc2b8c73a8726661ff5507fdaf63bc7f
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-06-28 14:45:35 +0300

    setting up paramemters and keys in both prod and dev environments

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+
+if(process.env.NODE_ENV === 'production'){
+    // we are in production -return the the dev keys !!!
+    module.exports = require('./prod');
+}
+else {
+    // we are in development - return the dev keys!!!
+    module.exports = require('./dev');
+}
