f31ec6c12ddf102f19a410935d8c18c4c2e7022a
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-04 00:17:59 +0300

    updates

--- client/src/compontents/App.js
+++ client/src/compontents/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hi there
-        </a>
-
-        {/* <a href="http://localhost:5000/auth/google"> Sign In with Google </a> */}
+       {/* <a href="http://localhost:5000/auth/google"> Sign In with Google </a> */}
         <a href="/auth/google"> Sign In with Google </a>
 
       </header>
