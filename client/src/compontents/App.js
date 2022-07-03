36f39e85b866ff21b9b1c736e44b189443640768
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-04 00:40:28 +0300

    setting up redux

--- client/src/compontents/App.js
+++ client/src/compontents/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hi there </h1>
        {/* <a href="http://localhost:5000/auth/google"> Sign In with Google </a> */}
         <a href="/auth/google"> Sign In with Google </a>
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import {createStore , applyMiddleware} from 'redux';
 import './index.css';
 import App from './compontents/App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(() => [],{}, applyMiddleware());
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store ={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>
 );
 
