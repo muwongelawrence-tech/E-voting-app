1afc7b64b0b8e1da6a1a31f04d949bb8c69f4805
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-03 23:41:37 +0300

    modifications

--- services/passport.js
+++ services/passport.js
@@ -25,14 +25,14 @@ passport.use( new GoogleStrategy({
 
    const existingUser = await  User.findOne({ googleId: profile.id });
 
-        if(existingUser){
-            // we already have a record with the given profile ID
-           return done(null, existingUser); // tells passport that we are done successfully
-        } 
-        
-        // we don't have a user record with this ID, make a new record.
-        const user = await new User({ googleId : profile.id }).save()
-        done(null, user);// tells passport that we are done successfully
+            if(existingUser){
+                // we already have a record with the given profile ID
+            return done(null, existingUser); // tells passport that we are done successfully
+            } 
+            
+            // we don't have a user record with this ID, make a new record.
+            const user = await new User({ googleId : profile.id }).save()
+            done(null, user);// tells passport that we are done successfully
         
     
    
