979a2457621f6404f5e567db8dd5dd1443b40496
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-04 13:55:31 +0300

    adding routing to the application

--- client/src/compontents/App.js
+++ client/src/compontents/App.js
@@ -1,15 +1,24 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Landing from './Landing';
+
+const Header = () => <h2>header</h2>
+const Dashboard = () => <h2>header</h2>
+const  SurveyNew = () => <h2>header</h2>
+//const Landing = () => <div>Landing...</div>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hi there </h1>
+    <div >
+       <BrowserRouter>
+           <Routes>
+               <Route path ="/landing" component = { Landing } />
+             </Routes>
+            
+        </BrowserRouter>
        {/* <a href="http://localhost:5000/auth/google"> Sign In with Google </a> */}
-        <a href="/auth/google"> Sign In with Google </a>
-
-      </header>
-    </div>
+        {/* <a href="/auth/google"> Sign In with Google </a> */}
+      </div>
   );
 }
 
