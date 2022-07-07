d7568fa39eb947eaeba3a2d6fe1928c752c9f409
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-07 14:29:36 +0300

    Adding authentication to the navbar

--- client/src/compontents/Header.js
+++ client/src/compontents/Header.js
@@ -4,11 +4,11 @@ function Header() {
   return (
     <nav>
     <div className="nav-wrapper">
-      <a href="#" className="brand-logo">Logo</a>
+      <a href="#" className="brand-logo">Emaily</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+        <li><a href="/auth/google">log in</a></li>
       </ul>
     </div>
   </nav>
