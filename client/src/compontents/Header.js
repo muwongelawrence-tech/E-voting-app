b7f65f53651e774b43142b9901dcddd848459681
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-07 14:26:39 +0300

    Adding a navbar using materialize-css

--- client/src/compontents/Header.js
+++ client/src/compontents/Header.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <nav>
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">Logo</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+  );
 }
 
 export default Header;
