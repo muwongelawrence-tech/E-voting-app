74a20a9ed47896cc350a8a72622ea1a2e8c5f549
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-09 22:43:48 +0300

    Getting the user

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
+const fetchUser = async () => {
+  const user =  await axios.get('/api/current_user');
 
 };
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/auth/google", { target: "http://localhost:5000/" })
+    createProxyMiddleware("/auth/google", { target: "http://localhost:5000/" }),
+    createProxyMiddleware("/api/*", { target: "http://localhost:5000/" })
   );
 };
