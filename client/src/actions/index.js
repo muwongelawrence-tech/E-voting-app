03cb82fa3415bf92c5a84cac43b8c2c02dd6060a
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-09 22:59:06 +0300

    dispatching an action using an action reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = async () => {
-  const user =  await axios.get('/api/current_user');
-
+const fetchUser =  () => {
+    return function(dispatch){
+     axios.get('/api/current_user')
+         .then(res => dispatch({ type: FETCH_USER , payload: res }));
+  
+    }
+  
+//   return {
+//     type: FETCH_USER,
+//     payload: request
+//   };
 };
