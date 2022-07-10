c94553e2f92ca6012badda323fbc8d25c20ce303
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-10 23:49:42 +0300

    more refactoring

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => 
- async dispatch => {
-    const res = await axios.get('/api/current_user')
-    dispatch({ type: FETCH_USER , payload: res.data });
+
+
+export const fetchUser = () => 
+  async dispatch => {
+    const res = await axios.get('/api/current_user');
+
+    dispatch({ type: FETCH_USER , payload: res });
   
   };
+
   
 //   return {
 //     type: FETCH_USER,
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -4,7 +4,7 @@ export default function( state = null , action ){
     console.log(action);
     switch(action.type){
         case FETCH_USER:
-            return action.payload;
+            return action.payload || false;
         default:
             return state;
     }
