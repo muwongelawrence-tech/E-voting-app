7e256730970e7286e9b2630b069597ef11c35c06
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-10 23:20:16 +0300

    dispatching actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,7 +4,7 @@ import { FETCH_USER } from './types';
 const fetchUser = () => 
  async dispatch => {
     const res = await axios.get('/api/current_user')
-    dispatch({ type: FETCH_USER , payload: res });
+    dispatch({ type: FETCH_USER , payload: res.data });
   
   };
   
