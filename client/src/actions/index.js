891abd950d329c9d19902c5ccdb13020c0200391
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-10 23:03:33 +0300

    Refactoring

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser =  () => {
-    return function(dispatch){
-     axios.get('/api/current_user')
-         .then(res => dispatch({ type: FETCH_USER , payload: res }));
+const fetchUser = () => 
+ async dispatch => {
+    const res = await axios.get('/api/current_user')
+    dispatch({ type: FETCH_USER , payload: res });
   
-    }
+  };
   
 //   return {
 //     type: FETCH_USER,
 //     payload: request
 //   };
-};
