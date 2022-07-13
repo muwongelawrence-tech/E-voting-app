00a81e9a62e88c036022a032a845dcfba6c5552c
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-13 17:25:03 +0300

    refactoring

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,7 +8,7 @@ export const fetchUser = () =>
   async dispatch => {
     const res = await axios.get('/api/current_user');
 
-    dispatch({ type: FETCH_USER , payload: res });
+    dispatch({ type: FETCH_USER , payload: res.data });
   
   };
 
