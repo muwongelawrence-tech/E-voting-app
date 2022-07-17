c75570e641cbc67a755e0c6941eff5c5743dc25a
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-17 23:49:26 +0300

    updates

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,10 +11,11 @@ import * as actions from '../actions';
 
 //  <a href="http://localhost:5000/auth/google"> Sign In with Google </a> 
 //  <a href="/auth/google"> Sign In with Google </a>
-//  <a href="/auth/google"> Sign In with Google </a>  
+//  <a href="/auth/google"> Sign In with Google </a> 
+//  <a href="/auth/google"> Sign In with Google </a> 
 
 function App(props) {
-  // call to the useffect
+  // call to the useffect to fecth user.
   useEffect(() => {
      props.fetchUser();
   });
