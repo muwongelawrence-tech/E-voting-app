ff04c8e735a6b51d91e650474f472f0cb2f0d460
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-18 18:55:33 +0300

    configuring environment files in production and development

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-function Header({ auth ,user }) {
+function Header({ auth }) {
 
    const renderContent = () => {
        switch(auth){
