76f9c6d88e31b6168e5e82e36623f4f1a2fd964d
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-20 18:48:14 +0300

    configuring the payments section

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 function Header({ auth }) {
 
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -3,9 +3,12 @@ import StripeCheckout from 'react-stripe-checkout';
 
 
 function Payments() {
+      debugger;
   return (
     <StripeCheckout
        amount ={500}
+       token = { token => console.log(token)}
+       stripeKey={ process.env.REACT_APP_STRIPE_KEY }
      />
   );
 }
