69b0327e222fc9a6442959cb29a6ebc1e11bea11
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-20 19:33:51 +0300

    configuring payment for the additional credits

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,8 +13,8 @@ function Header({ auth }) {
            return <li><a href="/auth/google">Login with Google</a></li>;
          default:
            return [
-            <li> <Payments /> </li>,
-            <li><a href="/api/logout">Logout</a></li>
+            <li key="1"> <Payments /> </li>,
+            <li key="2"><a href="/api/logout">Logout</a></li>
            ];
        }
    }
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -3,13 +3,17 @@ import StripeCheckout from 'react-stripe-checkout';
 
 
 function Payments() {
-      debugger;
+      
   return (
     <StripeCheckout
+       name="Emaily"
+       description='$5 for 5 email credits'
        amount ={500}
        token = { token => console.log(token)}
        stripeKey={ process.env.REACT_APP_STRIPE_KEY }
-     />
+     >
+        <button className='btn'> Add Credits </button>
+     </StripeCheckout>
   );
 }
 
